f8f57f83ebfccc0666ab8016e841995d06a83a0e
GopiKAkella <163084619+GopiKAkella@users.noreply.github.com>
2024-03-13 18:25:29 +1100

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	String sayHello() {
-		return "Hello World, from CQUni... Gopi Akella Signing In...!";
+		return "Hello World, from CQUni !";
 	}
 }
